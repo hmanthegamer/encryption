dceef53180e650670a7bf946987d68aeda1602a4
unknown <henry.balch1@marist.edu>
2017-10-05 16:12:02 -0400

    converting to numbers and back

---
+++ main.py
@@ -0,0 +1,23 @@
+
+#converts string of characters to a list of numbers
+def char_to_num(string):
+    string = string.lower()
+    key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    result = []
+    for char in string:
+        try:
+            result.append(key.index(char))
+        except ValueError:
+            continue
+    return (result)
+
+#converts list of numbers to string
+def num_to_char(num_list):
+    key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    result = []
+    for num in num_list:
+        result.append(key[num])
+    return "".join(result)
+
