b998d0e5a06b98b33ea1763a5b400a95e2fc9e54
unknown <henry.balch1@marist.edu>
2017-10-06 23:16:29 -0400

    code seperation, other enhancements, resolves #1

---
+++ encrypt.py
@@ -0,0 +1,24 @@
+import convert
+
+def e_1(text):
+    num_list = convert.char_to_num(text)
+    encrypt_key_short = input("Enter a encryption key(make it long):\n")
+    encrypt_key = encrypt_key_short
+    while len(encrypt_key) < len(num_list):
+        encrypt_key += encrypt_key
+    
+    for count, n in enumerate(num_list):
+        num_list[count] = n * int(encrypt_key[count])
+    
+    return num_list, encrypt_key_short
+
+def decrypt_1(num_list, encrypt_key):
+    while len(encrypt_key) < len(num_list):
+        encrypt_key += encrypt_key
+    
+    for count, n in enumerate(num_list):
+        num_list[count] = n // int(encrypt_key[count])
+    
+    return convert.num_to_char(num_list)
+
+
--- main.py
+++ main.py
@@ -1,33 +1,7 @@
+import encrypt
 
-#converts string of characters to a list of numbers
-def char_to_num(string):
-    string = string.lower()
-    key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-    result = []
-    for char in string:
-        try:
-            result.append(key.index(char))
-        except ValueError:
-            continue
-    return (result)
-
-#converts list of numbers to string
-def num_to_char(num_list):
-    key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-    result = []
-    for num in num_list:
-        result.append(key[num])
-    return "".join(result)
-
-def encrypt(num_list, encrypt_key):
-    for count, n in enumerate(num_list):
-        num_list[count] = n * int(encrypt_key[count])
-    return num_list
-
-def decrypt(num_list, encrypt_key):
-    for count, n in enumerate(num_list):
-        num_list[count] = n // int(encrypt_key[count])
-    return num_list
-
+a = input("Enter some text:\n")
+b,c = encrypt.e_1(a)
+print(b)
+print(c)
+print(encrypt.decrypt_1(b,c))
