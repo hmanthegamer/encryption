9e58790d6f2d1c4153146155ba948b0a85adb091
unknown <henry.balch1@marist.edu>
2017-10-08 16:15:00 -0400

    conversion key is now more esaliey chandged

--- convert.py
+++ convert.py
@@ -1,10 +1,14 @@
 
-#converts string of characters to a list of numbers
-def char_to_num(string):
-    string = string.lower()
+def conversion_key():
     key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
            " ", "'", "\"", ".", "?", "(", ")", "!"]
+    return key
+
+#converts string of characters to a list of numbers
+def char_to_num(string):
+    string = string.lower()
+    key = conversion_key()
     result = []
     for char in string:
         try:
@@ -15,9 +19,7 @@ def char_to_num(string):
 
 #converts list of numbers to string
 def num_to_char(num_list):
-    key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-           " ", "'", "\"", ".", "?", "(", ")", "!"]
+    key = conversion_key()
     result = []
     for num in num_list:
         result.append(key[num])
